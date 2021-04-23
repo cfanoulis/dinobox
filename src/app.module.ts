5a0fe100d6189969bbefdd2c2568ef84946b76c3
Charalampos Fanoulis <yo@fanoulis.dev>
2021-04-23 22:04:43 +0300

    feat: create people controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PeopleController } from './people/people.controller';
+import { PeopleService } from './people/people.service';
+import { PeopleModule } from './people/people.module';
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService]
+	imports: [PeopleModule],
+	controllers: [AppController, PeopleController],
+	providers: [AppService, PeopleService]
 })
 export class AppModule {}
---
+++ src/lib/enum/ErrorReplies.ts
@@ -0,0 +1,5 @@
+export const enum ErrorReplies {
+	BAD_REQUEST = 'Bad Request',
+	NOT_FOUND = 'Not Found',
+	NO_LOOKUP_DATA_PROVIDED = 'You must provide either a slackId or an email to lookup'
+}
